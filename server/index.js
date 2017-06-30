f1e85a92ea7c7eb8e3ad615b646d0b21d16874b0
Alan Zheng <aszheng12@gmail.com>
2017-06-30 00:25:59 -0700

    deploy

--- server/index.js
+++ server/index.js
@@ -1,20 +1,44 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const request = require('request');
+// const dotenv = require('dotenv').config();
+
+// var port = process.env.PORT || 3000;
+
+// var Order = require('../database-mongo');
+
+// var app = express();
+
+// app.use(express.static(__dirname + '/../react-client/dist'));
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+// var routes = require('./Routes/serverRoutes');
+// routes(app);
+
+// app.listen(3000, function() {
+//   console.log(`listening on port ${port}!`);
+// });  
+
 const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
 const dotenv = require('dotenv').config();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+var Order = require('../database-mongo/index.js');
+
+const app = express();
 
 var port = process.env.PORT || 3000;
 
-var Order = require('../database-mongo');
-
-var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(__dirname + '/../react-client/dist'));
-app.use(bodyParser.urlencoded({ extended: false }))
+
 
 var routes = require('./Routes/serverRoutes');
 routes(app);
 
-app.listen(3000, function() {
-  console.log(`listening on port ${port}!`);
-});  
+var server = app.listen(port, function() {
+  console.log(`Magical unicorns will arrive on port ${port}!`);
+});
