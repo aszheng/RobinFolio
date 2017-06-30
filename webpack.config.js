bbfeec3b82f1f674a35026f303ea25f5ffb5e74b
Alan Zheng <aszheng12@gmail.com>
2017-06-29 18:18:23 -0700

    Changes to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+var webpack = require('webpack');
 var path = require('path');
-var SRC_DIR = path.join(__dirname, '/react-client/src');
-var DIST_DIR = path.join(__dirname, '/react-client/dist');
+
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -13,9 +15,9 @@ module.exports = {
       {
         test : /\.jsx?/,
         include : SRC_DIR,
-        loader : 'babel-loader',      
+        loader : 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'stage-2']
        }
       }
     ]
